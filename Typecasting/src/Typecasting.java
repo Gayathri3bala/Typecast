933d4990acf97969add5794908b1523b32d51b4c
Gayathri3bala <98374454+Gayathri3bala@users.noreply.github.com>
2022-01-27 08:53:36 +0530

    Add files via upload

---
+++ Typecasting/src/Typecasting.java
@@ -0,0 +1,16 @@
+
+public class Typecasting {
+	public static void main(String args[]) {
+		System.out.println("Implicit Typecasting");
+		char a='g';
+		System.out.println("The Value of g is:"+a);
+		int b=a;
+		System.out.println("The Value of g is:"+b);
+		System.out.println("Explicit Typecasting");
+		double myDouble=15.08d;
+		int myInt=(int)myDouble;
+		System.out.println(myDouble);
+		System.out.println(myInt);
+		}
+
+}
